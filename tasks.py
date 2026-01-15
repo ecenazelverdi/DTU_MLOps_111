545c2fadfa0dff0455a6d64617e75230f6a66e0a
Ecenaz <ecenazelverdi@gmail.com>
2026-01-15 11:10:47 +0100

    ===New project commands added===
    feat: Add tasks for data download and model evaluation, and parameterize the training task with learning rate, batch size, and epochs. They needed to be checked after necessary scripts added to the branch. docker_build, build_docs and serve_docs also needs to be checked (Added two ToDo command lines for this)

--- tasks.py
+++ tasks.py
@@ -8,14 +8,34 @@ PYTHON_VERSION = "3.12"
 
 # Project commands
 @task
+def download_data(ctx: Context) -> None:
+    """Download data from Kaggle."""
+    ctx.run(f"uv run src/{PROJECT_NAME}/data.py download", echo=True, pty=not WINDOWS)
+
+@task
 def export_data(ctx: Context) -> None:
     """Export data to nnU-Net raw format."""
     ctx.run(f"uv run src/{PROJECT_NAME}/data.py nnunet-export", echo=True, pty=not WINDOWS)
 
+# ToDo: These two tasks needs to be updated according to the incoming training and evaluation scripts
 @task
-def train(ctx: Context) -> None:
+def train(ctx: Context, lr: float = 1e-4, batch_size: int = 4, epochs: int = 10) -> None:
     """Train model."""
-    ctx.run(f"uv run src/{PROJECT_NAME}/train.py", echo=True, pty=not WINDOWS)
+    ctx.run(
+        f"uv run src/{PROJECT_NAME}/train.py --lr {lr} --batch-size {batch_size} --epochs {epochs}", 
+        echo=True, 
+        pty=not WINDOWS
+    )
+
+@task
+def evaluate(ctx: Context, checkpoint: str = "latest") -> None:
+    """Evaluate model."""
+    ctx.run(
+        f"uv run src/{PROJECT_NAME}/evaluate.py --checkpoint {checkpoint}", 
+        echo=True, 
+        pty=not WINDOWS
+    )
+#################################################################################
 
 @task
 def test(ctx: Context) -> None:
@@ -23,6 +43,7 @@ def test(ctx: Context) -> None:
     ctx.run("uv run coverage run -m pytest tests/", echo=True, pty=not WINDOWS)
     ctx.run("uv run coverage report -m -i", echo=True, pty=not WINDOWS)
 
+# ToDo: These tasks needed to be checked
 @task
 def docker_build(ctx: Context, progress: str = "plain") -> None:
     """Build docker images."""
@@ -47,3 +68,4 @@ def build_docs(ctx: Context) -> None:
 def serve_docs(ctx: Context) -> None:
     """Serve documentation."""
     ctx.run("uv run mkdocs serve --config-file docs/mkdocs.yaml", echo=True, pty=not WINDOWS)
+#################################################################################
