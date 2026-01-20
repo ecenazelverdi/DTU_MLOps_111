6036f866c3c6c360134f520b8d150e735815a004
Ecenaz <ecenazelverdi@gmail.com>
2026-01-20 09:35:37 +0100

    ===Fixed ModuleNotFoundError: No module named 'dtu_mlops_111' ===
    The problem for "no module found" was due to main.py importing the package (dtu_mlops_111) from the src directory, which the build backend (uv_build) could not automatically discover without explicit configuration in pyproject.toml. This is now fixed.

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ from dtu_mlops_111.api import app
 
 if __name__ == "__main__":
     # Run the API server
-    # IMPORTANT: Run "uv pip install -e ." or "uv pip install ." before running this 
-    # script or it might not able to find "dtu_mlops_111"
     import uvicorn
     uvicorn.run(app, host="[IP_ADDRESS]", port=8000)
